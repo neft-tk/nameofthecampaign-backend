6d73a79dca2c995018da728a3e97f3e9c24e534f
unknown <ebussman@gmail.com>
2023-01-26 17:02:39 -0800

    adding more login/user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 async function getUsers(req, res) {
@@ -10,6 +12,51 @@ async function getUsers(req, res) {
     }
 }
 
+async function getSingleUser(req, res) {
+    try {
+        const userData = await User.findByPk(req.params.userId)
+        return res.status(200).json(userData)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: 'An error occured finding a single user', err })        
+    }
+}
+
+async function postUserLogin(req, res) {
+    try {
+        const foundUser = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!foundUser) {
+            return res.status(401).json({ msg: 'invalid email' });
+        } else if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+            return res.status(401).json({ msg: 'invalid password' });
+        } else {
+            const token = jwt.sign(
+                {
+                    id: foundUser.id,
+                    email: foundUser.email,
+                    username: foundUser.username,
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '14d',
+                }
+            );
+            return res.status(200).json({ token, user: foundUser })
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: 'A login error has occured' })      
+    }
+}
+
+
 module.exports = {
     getUsers,
+    getSingleUser,
+    postUserLogin
 };
